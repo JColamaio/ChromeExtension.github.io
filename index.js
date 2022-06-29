d565e9b584f92c6ffd13638778f2393e110394d8
JColamaio <jcolamaio@gmail.com>
2022-06-29 19:46:38 -0300

    refactor 1

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const Ul = document.getElementById("ulEl")
+let listItems = ""
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
+    myLeads.push(inputEl.value);
+    
+    inputEl.value = ""
+    renderLeads()
 })
+function    renderLeads(){
+for(let i = 0 ; i < myLeads.length; i++) {
+    // listItems += "<li><a target='_blank' href= '" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    listItems += `
+    <li>
+        <a target='_blank' href='${myLeads[i]}'> 
+        ${myLeads[i]}
+        </a>
+    </li>`//template string
+    console.log(listItems)
 
+
+
+
+
+    // another way to do the this is
+    //  const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+}
+
+ulEl.innerHTML = listItems
+}
 
 
 
